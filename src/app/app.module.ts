0cbeabafa08c33e33ff8f4f9ddb7ec616990a132
Ronal David Hoyos <ronaldavidhoyos@gmail.com>
2023-02-01 13:22:40 -0500

    Maquetación inicial y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { CuentasComponent } from './pages/cuentas/cuentas.component';
+import { MovimientosComponent } from './pages/movimientos/movimientos.component';
+import { ReportesComponent } from './pages/reportes/reportes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ClientesComponent,
+    CuentasComponent,
+    MovimientosComponent,
+    ReportesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
